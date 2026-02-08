903d60285d22550f5b2fb42903ddaf3d00a84388
Rubi07 <118752991+Rubi07@users.noreply.github.com>
2026-02-08 23:10:17 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
-const CACHE_NAME = 'jj-v1';
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['index.html'])));
+//const CACHE_NAME = 'jj-v1';
+//self.addEventListener('install', (e) => {
+ // e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['index.html'])));
 });
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+//self.addEventListener('fetch', (e) => {
+ // e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
+
